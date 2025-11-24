a29f9c7a77b74ad3846c01244b39b0af79b5c76f
Vangel22 <h.vangel22@gmail.com>
2025-11-24 20:15:46 +0100

    Initiate project

---
+++ backend/api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+const data = [
+  {
+    id: 1,
+    name: "Ace",
+  },
+  {
+    id: 2,
+    name: "Riste",
+  },
+  {
+    id: 3,
+    name: "Ceca",
+  },
+];
+
+app.get("/", (req, res) => {
+  return res.send(`This is an environment variable: ${process.env.TEST}`);
+});
+
+app.get("/test", (req, res) => {
+  return res.send(`Hello ${req.query.name}`);
+});
+
+app.get("/data", (req, res) => {
+  return res.send(data);
+});
+
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
+
+module.exports = app;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}`);
+      const resData = await res.json();
+      if (resData) {
+        setData(resData);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => await fetchData())();
+  }, []);
+
+  return (
+    <>
+      <h1>Connecting via Vercel</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {data && data.map((d) => <span key={d.id}>{d.name}</span>)}
+      </div>
+    </>
+  );
+}
+
+export default App;
