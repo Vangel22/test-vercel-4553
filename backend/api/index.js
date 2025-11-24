98421508aa141bb156fa4dc982d75ad0c48f1d3d
Vangel22 <h.vangel22@gmail.com>
2025-11-24 20:39:38 +0100

    Save origin

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "https://test-vercel-4553-tdtp.vercel.app",
+    origin: ["http://localhost:5173", "https://test-vercel-4553.vercel.app"],
   })
 );
 
